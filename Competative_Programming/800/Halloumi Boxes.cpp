f5d5cc56a71d3f6c522302355a933f09c74b28c0
unknown <paikpawan18@gmail.com>
2024-03-22 12:38:08 +0530

    created a file for the competative programming and solved [200~#include<bits/stdc++.h>

---
+++ Competative_Programming/800/Halloumi Boxes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool issorted(int arr[],int n){
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            return false;
+        }
+    }
+    return true;
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        int temp;
+        for(int i=0;i<n;i++){ 
+            cin>>temp;
+            arr[i]=temp;
+        }
+        if(k==1 && !issorted(arr,n)){
+           cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+}
